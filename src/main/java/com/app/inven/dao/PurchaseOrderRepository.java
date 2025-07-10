4b12cf456338ff416cb975f2038f5936670d9737
Shubham <shubhamshinde.link@gmail.com>
2025-07-10 09:55:11 +0530

    PurchaseOrderRepository

---
+++ src/main/java/com/app/inven/dao/PurchaseOrderRepository.java
@@ -0,0 +1,12 @@
+package com.app.inven.dao;
+
+import com.app.inven.pojo.PurchaseOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PurchaseOrderRepository {
+    public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
+        List<PurchaseOrder> findByStatus(String status);
+    }
+}
