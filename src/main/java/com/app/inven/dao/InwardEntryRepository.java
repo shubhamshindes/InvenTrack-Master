86c25f1d24a0052a9c547e09ff64d5935b6cb68c
Shubham <shubhamshinde.link@gmail.com>
2025-07-10 12:25:28 +0530

    Updated if InwardEntry By Product in InwardEntryRepository

--- src/main/java/com/app/inven/dao/InwardEntryRepository.java
+++ src/main/java/com/app/inven/dao/InwardEntryRepository.java
@@ -4,10 +4,14 @@ import com.app.inven.pojo.InwardEntry;
 import com.app.inven.pojo.Product;
 import com.app.inven.pojo.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface InwardEntryRepository extends JpaRepository<InwardEntry, Long> {
-    List<InwardEntry> findByProduct(Product product);
+//    List<InwardEntry> findByProduct(Product product);
     List<InwardEntry> findByPurchaseOrder(PurchaseOrder order);
+    @Query("SELECT i FROM InwardEntry i WHERE i.product = :product")
+    List<InwardEntry> findByProduct(@Param("product") Product product);
 }
